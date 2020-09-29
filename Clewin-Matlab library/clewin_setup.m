496c556ea2a02db107ce48a39e82f28de48b1891
Guy Pardo <guy.pardo@gmail.com>
2020-09-29 15:07:58 +0300

    fix bug in setup scripts

--- Clewin-Matlab library/clewin_setup.m
+++ Clewin-Matlab library/clewin_setup.m
@@ -1,8 +1,9 @@
 
 % run this from clewin once. with lib_path the path to the liberary folder
 % by default it is "Z:\Clewin-Matlab library" 
-n_chars = numel('clewin_setup.m');
-full_name =  matlab.desktop.editor.getActiveFilename;
+n_chars = numel('clewin_setup');
+% full_name =  matlab.desktop.editor.getActiveFilename;
+full_name =  mfilename('fullpath');
  lib_path = full_name(1:end-n_chars-1);
 % 
  excluded_path = strcat(lib_path,"\excluded_folder" );
@@ -11,4 +12,4 @@ addpath(genpath(lib_path));
 rmpath(genpath(excluded_path));
  savepath
 % 
-
+clearvars
--- Clewin-Matlab library/matlab_setup.m
+++ Clewin-Matlab library/matlab_setup.m
@@ -1,8 +1,9 @@
 
 % run this from matlab  
 
-n_chars = numel('matlab_setup.m');
-full_name =  matlab.desktop.editor.getActiveFilename;
+n_chars = numel('matlab_setup');
+%full_name =  matlab.desktop.editor.getActiveFilename;
+full_name = mfilename('fullpath');
  lib_path = full_name(1:end-n_chars-1);
 % 
 
